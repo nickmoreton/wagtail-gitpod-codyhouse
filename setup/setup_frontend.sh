1eceebd00afdf7f78874428dbf02843399e2e494
Nick Moreton <nickmoreton@me.com>
2021-02-23 14:57:39 +0000

    update

--- setup/setup_frontend.sh
+++ setup/setup_frontend.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-wget https://github.com/CodyHouse/codyhouse-framework/archive/master.zip && unzip master.zip
+if [ ! -d "frontend" ]
+then
+    wget https://github.com/CodyHouse/codyhouse-framework/archive/master.zip && unzip master.zip
+    mv codyhouse-framework-master frontend
+    rm master.zip
+    sed -i -e "s/var scriptsJsPath = 'main\/assets\/js';/var scriptsJsPath = '..\/cms\/static\/js';/g" frontend/gulpfile.js
+    sed -i -e "s/var cssFolder = 'main\/assets\/css';/var cssFolder = '..\/cms\/static\/css';/g" frontend/gulpfile.js
+    # cd frontend && npm install
+else
+    echo "EXITED: already run"
+fi
--- setup/setup_wagtail.sh
+++ setup/setup_wagtail.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-pip install wagtail &&
-wagtail start config &&
-mv config cms &&
-mv cms/config/settings cms &&
-mv cms/config/static cms &&
-mv cms/config/templates cms &&
-mv cms/config/* cms &&
-rm -R cms/config &&
-mv cms/manage.py . &&
-sed -i -e "s/config.settings.dev/cms.settings.dev/g" manage.py &&
-sed -i -e "s/'home',/'cms.home',/g" cms/settings/base.py &&
-sed -i -e "s/'search',/'cms.search',/g" cms/settings/base.py &&
-sed -i -e "s/WSGI_APPLICATION = 'config.wsgi.application'/WSGI_APPLICATION = 'cms.wsgi.application'/g" cms/settings/base.py &&
-sed -i -e "s/ROOT_URLCONF = 'config.urls'/ROOT_URLCONF = 'cms.urls'/g" cms/settings/base.py &&
-sed -i -e "s/from search import views as search_views/from cms.search import views as search_views/g" cms/urls.py &&
-python manage.py migrate &&
-echo "from django.contrib.auth import get_user_model; get_user_model().objects.create_superuser('admin', '', 'changeme')" | python manage.py shell
+if [ ! -d "cms" ]
+then
+    pip install wagtail
+    wagtail start config
+    mv config cms
+    mv cms/config/settings cms
+    mv cms/config/static cms
+    mv cms/config/templates cms
+    mv cms/config/* cms
+    rm -R cms/config
+    mv cms/manage.py .
+    sed -i -e "s/config.settings.dev/cms.settings.dev/g" manage.py
+    sed -i -e "s/'home',/'cms.home',/g" cms/settings/base.py
+    sed -i -e "s/'search',/'cms.search',/g" cms/settings/base.py
+    sed -i -e "s/WSGI_APPLICATION = 'config.wsgi.application'/WSGI_APPLICATION = 'cms.wsgi.application'/g" cms/settings/base.py
+    sed -i -e "s/ROOT_URLCONF = 'config.urls'/ROOT_URLCONF = 'cms.urls'/g" cms/settings/base.py
+    sed -i -e "s/from search import views as search_views/from cms.search import views as search_views/g" cms/urls.py
+    python manage.py migrate
+    echo "from django.contrib.auth import get_user_model; get_user_model().objects.create_superuser('admin', '', 'changeme')" | python manage.py shell
+else
+    echo "EXITED: already run"
+fi
